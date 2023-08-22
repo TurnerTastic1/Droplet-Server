d8aa4c412b04eed2b3e8308316bf8064f3d214b6
Turner Naef <Turner.Naef@gmail.com>
2023-08-22 11:37:40 -0600

    routes and basic hello world route

--- src/main/java/com/TCorp/FitNetServer/FitNetServerApplication.java
+++ src/main/java/com/TCorp/FitNetServer/FitNetServerApplication.java
@@ -2,8 +2,13 @@ package com.TCorp.FitNetServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class FitNetServerApplication {
 
 	public static void main(String[] args) {
@@ -11,4 +16,8 @@ public class FitNetServerApplication {
 		SpringApplication.run(FitNetServerApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public List<String> hello() {
+		return List.of("Hello World! API is currently v1.");
+	}
 }
