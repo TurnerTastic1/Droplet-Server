9daf8f91bbb9796cbcb139b6cfb991f83c9cc988
Turner Naef <Turner.Naef@gmail.com>
2023-08-23 16:29:17 -0600

    Janky first test

---
+++ src/test/java/com/TCorp/FitNetServer/RootHealthTest.java
@@ -0,0 +1,22 @@
+package com.TCorp.FitNetServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = FitNetServerApplication.class)
+public class RootHealthTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void getRoot_success() throws Exception {
+        System.out.println("Starting RootHealthTest...");
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/"))
+                .andExpect(status().isOk());
+    }
+}
