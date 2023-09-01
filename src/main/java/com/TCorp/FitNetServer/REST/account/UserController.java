afedc8639eaae7030d6143bb6203c926f22686aa
Turner Naef <Turner.Naef@gmail.com>
2023-09-01 14:13:38 -0600

    user controller basic, broken build

---
+++ src/main/java/com/TCorp/FitNetServer/REST/account/UserController.java
@@ -0,0 +1,15 @@
+package com.TCorp.FitNetServer.REST.account;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "user")
+public class UserController {
+
+    @GetMapping()
+    public String hello() {
+        return "Hello World from user controller!";
+    }
+}
